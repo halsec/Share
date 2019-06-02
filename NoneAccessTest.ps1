447f121b6f9b11390a1fb105ef9b354491e6c4b9
halsec <halsec@gmail.com>
2019-06-02 14:58:23 +0200

    1

--- NoneAccessTest.ps1
+++ NoneAccessTest.ps1
@@ -1,5 +1,12 @@
 $path = "C:\HAL\Powershell\Share\test\Share3"
 $acl = get-acl -LiteralPath $path
 $access = $acl.Access
-$access
+#$access | select IdentityReference,FileSystemRights,IsInherited
 
+$accessUnique = $access | ?{$_.IsInherited -eq "False"}
+$accessInherited = $access | ?{$_.IsInherited -eq "True"}
+
+write-host "Access Unique"
+$accessUnique | select IdentityReference,FileSystemRights,IsInherited
+write-host "Access Inherited"
+$accessInherited | select IdentityReference,FileSystemRights,IsInherited
