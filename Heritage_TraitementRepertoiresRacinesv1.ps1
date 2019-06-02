1142f71cdf8910126a85c67dcbba4cf128387a9d
halsec <halsec@gmail.com>
2019-06-02 21:19:53 +0200

    Done - Suppression des doublons

--- Heritage_TraitementRepertoiresRacinesv1.ps1
+++ Heritage_TraitementRepertoiresRacinesv1.ps1
@@ -34,17 +34,34 @@ Function supprDoublonsUnique {
     $access = $acl.Access
     #$access | select IdentityReference,FileSystemRights,IsInherited
     $accessInherited = $access | ?{ $_.IsInherited -eq $true}
-    $accessInherited | ft IdentityReference,FileSystemRights,IsInherited
+    #$accessInherited | ft IdentityReference,FileSystemRights,IsInherited
     Write-Host "---------------------------------------------------------" -ForegroundColor yellow
 
     $accessUnique = $access | ?{$_.IsInherited -eq $false}
-    $accessUnique | ft IdentityReference,FileSystemRights,IsInherited
+    #$accessUnique | ft IdentityReference,FileSystemRights,IsInherited
 
     $accessToRemove = Compare-Object -ReferenceObject $accessInherited -DifferenceObject $accessUnique  -Property IdentityReference,FileSystemRights -IncludeEqual
-    $accessToRemove | ?{$_.SideIndicator -eq "=="}
+    #$accessToRemove | ?{$_.SideIndicator -eq "=="}
 
     Write-Host "------------------ UNIQUE ---------------------------------------" -ForegroundColor yellow
     #$access | sort | select -Unique
+
+    #Suppression des permissions en double et non héritées
+    [System.Collections.ArrayList]$rules = @()
+    ForEach ($accessEnTrop in ($accessToRemove | ?{$_.SideIndicator -eq "=="}))
+    {
+        #("Identity {0}" -f $accessEnTrop.IdentityReference)
+        $rules += $acl.access | Where-Object { 
+            (-not $_.IsInherited) -and 
+            $_.IdentityReference -eq $accessEnTrop.IdentityReference
+        }
+    }
+    ForEach($rule in $rules) {
+        $acl.RemoveAccessRule($rule) | Out-Null
+        $rule
+    }
+    Set-ACL -Path $path -AclObject $acl
+
 }#process
  
  END {
--- NoneAccessTest.ps1
+++ NoneAccessTest.ps1
@@ -9,9 +9,25 @@ Write-Host "---------------------------------------------------------" -Foregrou
 $accessUnique = $access | ?{$_.IsInherited -eq $false}
 $accessUnique | ft IdentityReference,FileSystemRights,IsInherited
 
-$accessToRemove = Compare-Object -ReferenceObject $accessInherited -DifferenceObject $accessUnique  -Property IdentityReference,FileSystemRights -IncludeEqual
+$accessToRemove = Compare-Object -ReferenceObject $accessInherited -DifferenceObject $accessUnique -Property IdentityReference,FileSystemRights -IncludeEqual
 $accessToRemove | ?{$_.SideIndicator -eq "=="}
 
 Write-Host "------------------ UNIQUE ---------------------------------------" -ForegroundColor yellow
 #$access | sort | select -Unique
+
+#Suppression des permissions en double et non héritées
+[System.Collections.ArrayList]$rules = @()
+ForEach ($accessEnTrop in ($accessToRemove | ?{$_.SideIndicator -eq "=="}))
+{
+    #("Identity {0}" -f $accessEnTrop.IdentityReference)
+    $rules += $acl.access | Where-Object { 
+        (-not $_.IsInherited) -and 
+        $_.IdentityReference -eq $accessEnTrop.IdentityReference
+    }
+}
+ForEach($rule in $rules) {
+    $acl.RemoveAccessRule($rule) | Out-Null
+    $rule
+}
+Set-ACL -Path $path -AclObject $acl
 
